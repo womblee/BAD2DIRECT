fc8244a81550e3aabf908143412fa4e160ddaddd
womble <52250786+womblee@users.noreply.github.com>
2022-09-05 13:21:42 +0200

    Initial commit

---
+++ convert.py
@@ -0,0 +1,36 @@
+# Regular link
+# https://drive.google.com/file/d/FILE_ID/view?usp=sharing
+
+# Direct download link
+# https://drive.google.com/uc?id=FILE_ID&export=download&confirm=t
+
+# Import
+import argparse
+
+# Arguments
+parser = argparse.ArgumentParser(
+    description='BAD2DIRECT - Converts regular bad google drive links to direct ones')
+parser.add_argument('--link',
+    type=str, help='Google drive link')
+
+# Table
+args = parser.parse_args()
+
+# Link
+url = args.link
+
+# Validate
+if not isinstance(url, str):
+    raise Exception("URL is not a string, aborting...")
+
+# Extracting
+start = url.index('/d/')
+end = url.index('/view?', start + 3)
+
+# ID
+file_id = url[start + 3 : end]
+
+# Print
+message = "https://drive.google.com/uc?id=%s&export=download&confirm=t" % file_id
+
+print(message)
